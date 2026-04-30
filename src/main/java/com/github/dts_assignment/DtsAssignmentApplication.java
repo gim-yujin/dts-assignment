0947f24e30db729814613dcfe2a6b39c24311781
gim-yujin <gim.yujin.dev@gmail.com>
2026-05-01 07:56:57 +0900

    init: project scaffold

---
+++ src/main/java/com/github/dts_assignment/DtsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.dts_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtsAssignmentApplication.class, args);
+	}
+
+}
